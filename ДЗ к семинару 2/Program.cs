9511077fb3980a4e3ce93bf1692ece47914850d2
Vadik <gloom16@mail.ru>
2022-10-08 18:39:40 +0700

    Решил задачу 13

--- ДЗ к семинару 2/Program.cs
+++ ДЗ к семинару 2/Program.cs
@@ -13,5 +13,35 @@
 // Console.WriteLine($"Показывает второе число {num2} ");
 
 
+// Задача 13
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+// Решаем с помощью "/" и "%", со строками не работаем (Максимальное число 100000)
 
+// РЕШЕНИЕ
+
+// Console.WriteLine("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// int num1 = 999;
+// int num2 = 9999;
+// int numMin = 100;
+// int numMax = 99999;
+// if(number < numMin){
+//     Console.WriteLine($"Число {number} не имеет третьей цифры ");
+// }
+// if(number > numMax){
+//     Console.WriteLine($"Число {number} больше максимального значения ");
+// }
+// int number3 = 0;
+// if(numMin <= number && number <= num1){
+//     number3 = (number % 100) % 10;
+//     Console.WriteLine($"Третье цифра в числе {number} является {number3}");
+// }
+// if(number > num1 && number <= num2){
+//     number3 = (number % 100) / 10;
+//     Console.WriteLine($"Третье цифра в числе {number} является {number3}");
+// }
+// if(number > num2 && number <= numMax){
+//     number3 = (number % 1000) / 100;
+//     Console.WriteLine($"Третье цифра в числе {number} является {number3}");
+// }
 
